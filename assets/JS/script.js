e6f16c8a798fb86f3f622ca94e6ab8ccbd6597e8
hs127 <39209601+hs127@users.noreply.github.com>
2019-11-09 18:34:33 -0600

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,89 @@
+// $(document).ready(function () {
+//     $("#Trivia").hide();
+//     $("#TasteBuds").hide();
+//     $("#Train").hide();
+//     $("#pychic").hide();
+//     $("#deets").hide();
+
+//     console.log("ready");
+
+
+//     $(document).on('click', '.appTitle', function (e) {
+//         //     console.log("user clicked");
+//         $("#deets").show();
+
+
+//         var titleVal = $(e.target).attr("data-attr");
+//         $("#projTitle").text(titleVal);
+
+//         var titleVal = $(e.target).attr("data-attr");
+
+//         if (titleVal === "Harry Potter Trivia Game") {
+//             $("#Trivia").show();
+//             $("#TasteBuds").hide();
+//             $("#Train").hide();
+//             $("#pychic").hide();
+//         }
+        //     else if (titleVal === "TasteBuds") {
+        //         $("#Trivia").hide();
+        //         $("#TasteBuds").show();
+        //         $("#Train").hide();
+        //         $("#pychic").hide();
+        //     }
+        //     else if (titleVal === "Train") {
+        //         $("#Trivia").hide();
+        //         $("#TasteBuds").hide();
+        //         $("#Train").show();
+        //         $("#pychic").hide();
+        //     }
+        //     else if (titleVal === "pychic") {
+        //         $("#Trivia").hide();
+        //         $("#TasteBuds").hide();
+        //         $("#Train").hide();
+        //         $("#pychic").show();
+        //     }
+
+
+//     });
+
+
+
+
+//     $(document).on('click', '.hp', function (e) {
+//         console.log("hp clicked");
+//         clear();
+//         $("#deets").show();
+//         $("#Trivia").show();
+//         $("#TasteBuds").hide();
+//         $("#Train").hide();
+//         $("#pychic").hide();
+//     });
+
+
+//     $(document).on('click', '.tb', function (e) {
+//         console.log("hp clicked");
+//         //clear();
+//         $("#deets").show();
+//         $("#Trivia").hide();
+//         $("#TasteBuds").show();
+//         $("#Train").hide();
+//         $("#pychic").hide();
+//     });
+
+//     function clear() {
+//         $("#deets").clear();
+//     }
+
+
+
+
+//     // $("#blue").click(function () {
+//     //     //alert("this works"); 
+//     //     userGuessTotal = stoneCollection.sapphire.value + userGuessTotal;
+//     //     console.log("testing blue : " + userGuessTotal);
+//     //     $("#totalScore").text(userGuessTotal);
+//     //     // checker();
+//     // });
+
+
+// }); 
